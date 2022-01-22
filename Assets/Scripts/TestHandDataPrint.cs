c4c3c15ffc2f11be450c2fcbc13eb7e27ae53fe4
Kvan7 <71402892+Kvan7@users.noreply.github.com>
2022-01-22 14:36:34 -0600

    Update formatting

--- Assets/Scripts/TestHandDataPrint.cs
+++ Assets/Scripts/TestHandDataPrint.cs
@@ -6,69 +6,69 @@ using Leap.Unity;
 
 public class TestHandDataPrint : MonoBehaviour
 {
-  Controller controller;
-  float HandPalmPitch;
-  float HandPalmYaw;
-  float HandPalmRoll;
-  float HandWristRot;
-  float HandDirectionPitch;
-  float pinch;
-  float HandPalmDirectionRoll;
-  float LeftHand;
-  float RightHand;
-  float DistanceBetweenHands;
-  Vector VectorHandPosRight;
-  Vector VectorHandPosLeft;
+	Controller controller;
+	float HandPalmPitch;
+	float HandPalmYaw;
+	float HandPalmRoll;
+	float HandWristRot;
+	float HandDirectionPitch;
+	float pinch;
+	float HandPalmDirectionRoll;
+	float LeftHand;
+	float RightHand;
+	float DistanceBetweenHands;
+	Vector VectorHandPosRight;
+	Vector VectorHandPosLeft;
 
-  // Start is called before the first frame update
-  void Start()
-  {
+	// Start is called before the first frame update
+	void Start()
+	{
 
-  }
+	}
 
-  // Update is called once per frame
-  void Update()
-  {
-    if (Input.GetKeyDown(KeyCode.Space))
-    {
-      controller = new Controller();
-      Frame frame = controller.Frame();
-      List<Hand> hands = frame.Hands;
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			controller = new Controller();
+			Frame frame = controller.Frame();
+			List<Hand> hands = frame.Hands;
 
-      if (frame.Hands.Count > 0)
-      {
-        Hand firstHand = hands[0];
-        Hand secondHand = hands[1];
-      }
+			if (frame.Hands.Count > 0)
+			{
+				Hand firstHand = hands[0];
+				Hand secondHand = hands[1];
+			}
 
-      HandPalmPitch = hands[0].PalmNormal.Pitch;
-      HandPalmRoll = hands[0].PalmNormal.Roll;
-      HandPalmYaw = hands[0].PalmNormal.Yaw;
+			HandPalmPitch = hands[0].PalmNormal.Pitch;
+			HandPalmRoll = hands[0].PalmNormal.Roll;
+			HandPalmYaw = hands[0].PalmNormal.Yaw;
 
-      VectorHandPosRight = hands[0].PalmPosition; //position right hand
-      VectorHandPosLeft = hands[1].PalmPosition; // position left hand
+			VectorHandPosRight = hands[0].PalmPosition; //position right hand
+			VectorHandPosLeft = hands[1].PalmPosition; // position left hand
 
-      DistanceBetweenHands = VectorHandPosLeft.DistanceTo(VectorHandPosRight); //distance position between hands as float
+			DistanceBetweenHands = VectorHandPosLeft.DistanceTo(VectorHandPosRight); //distance position between hands as float
 
-      RightHand = hands[0].PalmNormal.Roll; //clap right hand
-      LeftHand = hands[1].PalmNormal.Roll; // clap left hand
+			RightHand = hands[0].PalmNormal.Roll; //clap right hand
+			LeftHand = hands[1].PalmNormal.Roll; // clap left hand
 
-      HandPalmDirectionRoll = hands[0].Direction.Roll; //stroke
+			HandPalmDirectionRoll = hands[0].Direction.Roll; //stroke
 
-      pinch = hands[0].PinchStrength; //turn radio on
+			pinch = hands[0].PinchStrength; //turn radio on
 
 
-      Debug.Log("RightHand: " + RightHand);
-      Debug.Log("LeftHand: " + LeftHand);
-      Debug.Log("Pitch: " + HandPalmPitch);
-      Debug.Log("Roll: " + HandPalmRoll);
-      Debug.Log("Yaw: " + HandPalmYaw);
-      Debug.Log("Wrist: " + HandWristRot);
-      Debug.Log("Pinch: " + pinch);
-      Debug.Log("PalmdirectionRoll: " + HandPalmDirectionRoll);
-      Debug.Log("RightHandPos: " + VectorHandPosRight);
-      Debug.Log("LeftHandPos: " + VectorHandPosLeft);
-      Debug.Log("DistanceBetween: " + DistanceBetweenHands);
-    }
-  }
+			Debug.Log("RightHand: " + RightHand);
+			Debug.Log("LeftHand: " + LeftHand);
+			Debug.Log("Pitch: " + HandPalmPitch);
+			Debug.Log("Roll: " + HandPalmRoll);
+			Debug.Log("Yaw: " + HandPalmYaw);
+			Debug.Log("Wrist: " + HandWristRot);
+			Debug.Log("Pinch: " + pinch);
+			Debug.Log("PalmdirectionRoll: " + HandPalmDirectionRoll);
+			Debug.Log("RightHandPos: " + VectorHandPosRight);
+			Debug.Log("LeftHandPos: " + VectorHandPosLeft);
+			Debug.Log("DistanceBetween: " + DistanceBetweenHands);
+		}
+	}
 }
