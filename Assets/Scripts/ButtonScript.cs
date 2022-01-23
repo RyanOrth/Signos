93c070d91ef03527b55efff7d5004e9cb9c7cb40
zstanczyk0803 <zstanczyk0803@gmail.com>
2022-01-23 09:13:51 -0600

    Updated button scrips for hand selection.

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -11,16 +11,23 @@ public class ButtonScript : MonoBehaviour
     public GameObject progressBar;
     public GameObject handRender;
     public GameObject handSettings;
+    public Button polyHand;
+    public Button skeletonHand;
+    public Button glowHand;
+    public Button ghostHand;
+
 
     public void StartGame()
     { 
         SceneManager.LoadScene("Prefab Making");
+        LowPolyHandSelect();
     }
 
     public void ExitGame()
     {
         Application.Quit(-1);
     }
+
     public void TogglePanel()
     {
         panel.SetActive(!panel.activeSelf);
@@ -50,20 +57,36 @@ public class ButtonScript : MonoBehaviour
     public void LowPolyHandSelect()
     {
         System.IO.File.WriteAllText(Application.persistentDataPath + "/handType.txt", "0");
+        polyHand.interactable = false;
+        skeletonHand.interactable = true;
+        glowHand.interactable = true;
+        ghostHand.interactable = true;
     }
 
     public void SkeletonHandSelect()
     {
         System.IO.File.WriteAllText(Application.persistentDataPath + "/handType.txt", "1");
+        polyHand.interactable = true;
+        skeletonHand.interactable = false;
+        glowHand.interactable = true;
+        ghostHand.interactable = true;
     }
 
     public void GlowHandSelect()
     {
         System.IO.File.WriteAllText(Application.persistentDataPath + "/handType.txt", "2");
+        polyHand.interactable = true;
+        skeletonHand.interactable = true;
+        glowHand.interactable = false;
+        ghostHand.interactable = true;
     }
 
     public void GhostHandSelect()
     {
         System.IO.File.WriteAllText(Application.persistentDataPath + "/handType.txt", "3");
+        polyHand.interactable = true;
+        skeletonHand.interactable = true;
+        glowHand.interactable = true;
+        ghostHand.interactable = false;
     }
 }
