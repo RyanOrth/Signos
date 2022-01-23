438cee41f38e5dcbc2c88bd0b25332744ae6cd75
rorth3 <rorth3@huskers.unl.edu>
2022-01-22 18:51:49 -0600

    Added Record Data functionality

--- Assets/Scripts/TestHandDataPrint.cs
+++ Assets/Scripts/TestHandDataPrint.cs
@@ -4,144 +4,170 @@ using UnityEngine;
 using Leap;
 using Leap.Unity;
 
+using System.IO;
+
 public class TestHandDataPrint : MonoBehaviour
 {
-  Controller controller;
-  void Start()
-  {
+	Controller controller;
+	void Start()
+	{
 
-  }
+	}
 
-  void Update()
-  {
-    if (Input.GetKeyDown(KeyCode.Space))
-    {
-      controller = new Controller();
-      Frame frame = controller.Frame();
-      List<Hand> hands = frame.Hands;
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			controller = new Controller();
+			Frame frame = controller.Frame();
+			List<Hand> hands = frame.Hands;
 
-      for (int i = 0; i < frame.Hands.Count; i++)
-      {
-        string handName = hands[i].IsRight ? "Right Hand" : "Left Hand";
-        PrintHandData(hands[i], handName);
-      }
-    }
-  }
+			// for (int i = 0; i < frame.Hands.Count; i++)
+			// {
+			//   string handName = hands[i].IsRight ? "Right Hand" : "Left Hand";
+			//   PrintHandData(hands[i], handName);
+			// }
+			RecordData(hands[0]);
 
-  void PrintHandData(Hand hand, string handName)
-  {
-    int HandId = hand.Id;
-    float confidence = hand.Confidence;
+		}
+	}
 
-    List<Finger> Fingers = hand.Fingers;
+	void RecordData(Hand hand)
+	{
+		Debug.Log("Record Data Function");
+		if (!File.Exists("data.txt"))
+		{
+			FileStream fileStreamInit = new FileStream(@"data.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+			string headerText = "HandId,HandConfidence,HandPalmNormalPitch,HandPalmNormalRoll,"
+			 + "HandPalmDirectionRoll,HandPalmPosition,HandPalmVelocity,HandPalmNormal,"
+			 + "HandDirection,HandRotation,GrabStrength,GrabAngle,PinchDistance,PinchStrength,"
+			 + "PalmWidth,StabilizedPalmPosition,TimeVisible,HandWristPosition\n";
+			File.AppendAllText("data.txt", headerText);
+		}
 
-    float HandPalmPitch = hand.PalmNormal.Pitch;
-    float HandPalmRoll = hand.PalmNormal.Roll;
-    float HandPalmYaw = hand.PalmNormal.Yaw;
-    float HandPalmDirectionRoll = hand.Direction.Roll;
+		string handData = "" + hand.Id + "," + hand.Confidence + "," + hand.PalmNormal.Pitch
+		+ "," + hand.PalmNormal.Roll + "," + hand.PalmNormal.Yaw + "," + hand.Direction.Roll
+		+ "," + hand.PalmPosition + "," + hand.PalmVelocity + "," + hand.PalmNormal + ","
+		+ hand.Direction + "," + hand.Rotation + "," + hand.GrabStrength + "," + hand.GrabAngle
+		+ "," + hand.PinchDistance + "," + hand.PinchStrength + "," + hand.PalmWidth + ","
+		+ hand.StabilizedPalmPosition + "," + hand.TimeVisible + "," + hand.WristPosition + "\n";
+		File.AppendAllText("data.txt", handData);
+	}
 
-    Vector HandPalmPosition = hand.PalmPosition;
-    Vector HandPalmVelocity = hand.PalmVelocity;
-    Vector HandPalmNormal = hand.PalmNormal;
-    Vector HandFingersDirection = hand.Direction;
+	void PrintHandData(Hand hand, string handName)
+	{
+		int HandId = hand.Id;
+		float confidence = hand.Confidence;
 
-    LeapQuaternion HandRotation = hand.Rotation;
+		List<Finger> Fingers = hand.Fingers;
 
-    float GrabStrength = hand.GrabStrength;
-    float GrabAngle = hand.GrabAngle;
+		float HandPalmPitch = hand.PalmNormal.Pitch;
+		float HandPalmRoll = hand.PalmNormal.Roll;
+		float HandPalmYaw = hand.PalmNormal.Yaw;
+		float HandPalmDirectionRoll = hand.Direction.Roll;
 
-    float PinchDistance = hand.PinchDistance;
-    float PinchStrength = hand.PinchStrength;
+		Vector HandPalmPosition = hand.PalmPosition;
+		Vector HandPalmVelocity = hand.PalmVelocity;
+		Vector HandPalmNormal = hand.PalmNormal;
+		Vector HandFingersDirection = hand.Direction;
 
-    float PalmWidth = hand.PalmWidth;
+		LeapQuaternion HandRotation = hand.Rotation;
 
-    Vector StabilizedPalmPosition = hand.StabilizedPalmPosition;
+		float GrabStrength = hand.GrabStrength;
+		float GrabAngle = hand.GrabAngle;
 
-    float TimeVisible = hand.TimeVisible;
+		float PinchDistance = hand.PinchDistance;
+		float PinchStrength = hand.PinchStrength;
 
-    Vector HandWristPos = hand.WristPosition;
+		float PalmWidth = hand.PalmWidth;
 
-    Debug.Log(handName + " Hand ID: " + HandId);
-    Debug.Log(handName + " Confidence: " + confidence);
-    Debug.Log(handName + " Fingers: " + Fingers);
-    Debug.Log(handName + " Pitch: " + HandPalmPitch);
-    Debug.Log(handName + " Roll: " + HandPalmRoll);
-    Debug.Log(handName + " Yaw: " + HandPalmYaw);
-    Debug.Log(handName + " PalmDirectionRoll: " + HandPalmDirectionRoll);
+		Vector StabilizedPalmPosition = hand.StabilizedPalmPosition;
 
-    Debug.Log(handName + " PalmPosition: " + HandPalmPosition);
-    Debug.Log(handName + " PalmVelocity: " + HandPalmVelocity);
-    Debug.Log(handName + " PalmNormal: " + HandPalmNormal);
-    Debug.Log(handName + " FingersDirection: " + HandFingersDirection);
+		float TimeVisible = hand.TimeVisible;
 
-    Debug.Log(handName + " HandRotation: " + HandRotation);
+		Vector HandWristPos = hand.WristPosition;
 
-    Debug.Log(handName + " GrabStrength: " + GrabStrength);
-    Debug.Log(handName + " GrabAngle: " + GrabAngle);
+		Debug.Log(handName + " Hand ID: " + HandId);
+		Debug.Log(handName + " Confidence: " + confidence);
+		Debug.Log(handName + " Fingers: " + Fingers);
+		Debug.Log(handName + " Pitch: " + HandPalmPitch);
+		Debug.Log(handName + " Roll: " + HandPalmRoll);
+		Debug.Log(handName + " Yaw: " + HandPalmYaw);
+		Debug.Log(handName + " PalmDirectionRoll: " + HandPalmDirectionRoll);
 
-    Debug.Log(handName + " Pinch Distance: " + PinchDistance);
-    Debug.Log(handName + " PinchStrength: " + PinchStrength);
+		Debug.Log(handName + " PalmPosition: " + HandPalmPosition);
+		Debug.Log(handName + " PalmVelocity: " + HandPalmVelocity);
+		Debug.Log(handName + " PalmNormal: " + HandPalmNormal);
+		Debug.Log(handName + " FingersDirection: " + HandFingersDirection);
 
-    Debug.Log(handName + " PalmWidth: " + PalmWidth);
+		Debug.Log(handName + " HandRotation: " + HandRotation);
 
-    Debug.Log(handName + " StabilizedPalmPosition: " + StabilizedPalmPosition);
+		Debug.Log(handName + " GrabStrength: " + GrabStrength);
+		Debug.Log(handName + " GrabAngle: " + GrabAngle);
 
-    Debug.Log(handName + " TimeVisible: " + TimeVisible);
+		Debug.Log(handName + " Pinch Distance: " + PinchDistance);
+		Debug.Log(handName + " PinchStrength: " + PinchStrength);
 
-    Debug.Log(handName + " WristPos: " + HandWristPos);
-  }
+		Debug.Log(handName + " PalmWidth: " + PalmWidth);
 
-  void PrintFingerData(Finger finger)
-  {
-    float FingerId = finger.Id;
-    Finger.FingerType FingerType = finger.Type;
-    int HandId = finger.HandId;
+		Debug.Log(handName + " StabilizedPalmPosition: " + StabilizedPalmPosition);
 
-    Vector TipPosition = finger.TipPosition;
-    Vector Direction = finger.Direction;
+		Debug.Log(handName + " TimeVisible: " + TimeVisible);
 
-    float Width = finger.Width;
-    float Length = finger.Length;
+		Debug.Log(handName + " WristPos: " + HandWristPos);
+	}
 
-    bool IsExtended = finger.IsExtended;
-    float TimeVisible = finger.TimeVisible;
+	void PrintFingerData(Finger finger)
+	{
+		float FingerId = finger.Id;
+		Finger.FingerType FingerType = finger.Type;
+		int HandId = finger.HandId;
 
-    Debug.Log("Finger ID: " + FingerId);
-    Debug.Log("Finger Type: " + FingerType);
-    Debug.Log("Hand ID: " + HandId);
-    Debug.Log("Tip Position: " + TipPosition);
-    Debug.Log("Direction: " + Direction);
-    Debug.Log("Width: " + Width);
-    Debug.Log("Length: " + Length);
-    Debug.Log("IsExtended: " + IsExtended);
-    Debug.Log("TimeVisible: " + TimeVisible);
+		Vector TipPosition = finger.TipPosition;
+		Vector Direction = finger.Direction;
 
-  }
+		float Width = finger.Width;
+		float Length = finger.Length;
 
-  void PrintBoneData(Bone bone)
-  {
-    Vector PrevJoint = bone.PrevJoint;  //base of the bone closest to wrist
-    Vector BoneMidpoint = bone.Center;
-    Vector NextJoint = bone.NextJoint;  //end of the bone, closest to finger tip
-    Vector Direction = bone.Direction;
+		bool IsExtended = finger.IsExtended;
+		float TimeVisible = finger.TimeVisible;
 
-    float Length = bone.Length;
-    float Width = bone.Width;
-    // BoneType boneType = bone.Type;
+		Debug.Log("Finger ID: " + FingerId);
+		Debug.Log("Finger Type: " + FingerType);
+		Debug.Log("Hand ID: " + HandId);
+		Debug.Log("Tip Position: " + TipPosition);
+		Debug.Log("Direction: " + Direction);
+		Debug.Log("Width: " + Width);
+		Debug.Log("Length: " + Length);
+		Debug.Log("IsExtended: " + IsExtended);
+		Debug.Log("TimeVisible: " + TimeVisible);
 
-    LeapQuaternion Rotation = bone.Rotation;
-    LeapTransform basis = bone.Basis;
+	}
 
-    Debug.Log("PrevJoint: " + PrevJoint);
-    Debug.Log("BoneMidpoint: " + BoneMidpoint);
-    Debug.Log("NextJoint: " + NextJoint);
-    Debug.Log("Direction: " + Direction);
+	void PrintBoneData(Bone bone)
+	{
+		Vector PrevJoint = bone.PrevJoint;  //base of the bone closest to wrist
+		Vector BoneMidpoint = bone.Center;
+		Vector NextJoint = bone.NextJoint;  //end of the bone, closest to finger tip
+		Vector Direction = bone.Direction;
 
-    Debug.Log("Length: " + Length);
-    Debug.Log("Width: " + Width);
+		float Length = bone.Length;
+		float Width = bone.Width;
+		// BoneType boneType = bone.Type;
 
-    Debug.Log("Rotation: " + Rotation);
-    Debug.Log("Basis: " + basis);
+		LeapQuaternion Rotation = bone.Rotation;
+		LeapTransform basis = bone.Basis;
 
-  }
+		Debug.Log("PrevJoint: " + PrevJoint);
+		Debug.Log("BoneMidpoint: " + BoneMidpoint);
+		Debug.Log("NextJoint: " + NextJoint);
+		Debug.Log("Direction: " + Direction);
+
+		Debug.Log("Length: " + Length);
+		Debug.Log("Width: " + Width);
+
+		Debug.Log("Rotation: " + Rotation);
+		Debug.Log("Basis: " + basis);
+
+	}
 }
