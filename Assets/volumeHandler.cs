d86b2f7481d99f2957481cc23b4f7d341d810557
Eric Dundas <kuribodragon@gmail.com>
2022-01-23 09:19:08 -0600

    fixed a bug with files not being created

--- Assets/volumeHandler.cs
+++ Assets/volumeHandler.cs
@@ -8,8 +8,11 @@ public class volumeHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<AudioSource>().volume = Convert.ToSingle(System.IO.File.ReadAllText(Application.persistentDataPath + "/volumeLevel.txt"));
-    }
+        if (System.IO.File.Exists(Application.persistentDataPath + "/volumeLevel.txt"))
+        {
+            gameObject.GetComponent<AudioSource>().volume = Convert.ToSingle(System.IO.File.ReadAllText(Application.persistentDataPath + "/volumeLevel.txt"));
+        }
+        
 
     // Update is called once per frame
     void Update()
